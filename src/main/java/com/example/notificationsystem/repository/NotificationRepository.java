2d0719d8292dbb937c9749d652fdaa01d746b40b
Turat <torotura@icloud.com>
2024-11-13 22:19:11 +0600

    все с 0

--- src/main/java/com/example/notificationsystem/repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.notificationsystem.repository;
-
-import com.example.notificationsystem.model.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-}
--- src/main/java/com/example/notificationsystem/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.notificationsystem.repository;
-
-import com.example.notificationsystem.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
